2b9c8c32ff73847079e55d8f7c9e330fc2c125ca
Wiktor <Wiktor>
2016-05-24 20:20:06 +0200

    Dodano szablon oraz stworzono controller i service w AngularJS do obsługi logowania

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,7 +2,11 @@
 
 var app = angular.module('postmanManager', [
   'postmanControllers',
+  'dashboardControllers',
+  'userControllers',
   'postmanServices',
+  'userServices',
+  'authorizationServices',
   'postmanDirectives',
   'ngRoute'
 ]);
@@ -11,8 +15,12 @@ app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/postman/new', {
       templateUrl: '/MobilePost/web/assets/partials/postman/postman-form.html',
       controller: 'CreatePostmanForm'
+    }).when('/login',{
+      templateUrl: '/MobilePost/web/assets/partials/user/login-form.html',
+      controller: 'LoginUserForm'
     }).otherwise({
       templateUrl: '/MobilePost/web/assets/partials/index.html',
+      controller: 'Dashboard'
     });
   }
 ]);
--- web/assets/js/services.js
+++ web/assets/js/services.js
@@ -1,12 +1,30 @@
 'use strict';
 
 var postmanServices = angular.module('postmanServices', ['ngResource']);
+var userServices = angular.module('userServices', ['ngResource']);
+var authorizationServices = angular.module('authorizationServices', ['ngResource']);
 
 postmanServices.factory('Postman', ['$resource',
   function ($resource) {
     return $resource('http://pai.local/MobilePost/web/app_dev.php/api/postman/postmen/:postmanId.json', {}, {
       save: {method: 'POST', headers: {'Accept': 'application/json'}},
       get: {method: 'GET', isArray: false, headers: {'Accept': 'application/json'}}
+    });
+  }
+]);
+
+userServices.factory('User', ['$resource',
+  function($resource) {
+    return $resource('http://pai.local/MobilePost/web/app_dev.php/login_check', {}, {
+      login: {method: 'POST', headers: {'Accept': 'application/json'}}
+    });
+  }
+]);
+
+authorizationServices.factory('Authorization', ['$resource',
+  function($resource) {
+    return $resource('http://pai.local/MobilePost/web/app_dev.php/api/user.json', {}, {
+      get: {method: 'get', headers: {'Accept': 'application/json'}}
     });
   }
 ]);
